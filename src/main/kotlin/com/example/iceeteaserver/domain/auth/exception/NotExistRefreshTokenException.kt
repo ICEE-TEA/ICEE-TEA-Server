9f8dedd00a0ec8982c2edaffcf1ec3c286d5aaba
KimGyeongsuuu <sk62628504@gmail.com>
2023-04-08 14:02:35 +0900

    :sparkles: 예외처리 작성

---
+++ src/main/kotlin/com/example/iceeteaserver/domain/auth/exception/NotExistRefreshTokenException.kt
@@ -0,0 +1,7 @@
+package com.example.iceeteaserver.domain.auth.exception
+
+import com.example.iceeteaserver.global.error.ErrorCode
+import com.example.iceeteaserver.global.error.exceptions.BusinessException
+
+class NotExistRefreshTokenException : BusinessException(ErrorCode.NOT_EXIST_REFRESHTOKEN) {
+}
