f4a4407800bafae2072e8d61c49e6144f5b0f553
KimGyeongsuuu <sk62628504@gmail.com>
2023-05-20 11:33:51 +0900

    :sparkles: 토큰 재발급 구현

--- src/main/kotlin/com/example/iceeteaserver/domain/auth/service/impl/TokenReissueService.kt
+++ src/main/kotlin/com/example/iceeteaserver/domain/auth/service/impl/TokenReissueService.kt
@@ -3,32 +3,32 @@ package com.example.iceeteaserver.domain.auth.service.impl
 import com.example.iceeteaserver.domain.auth.presentation.dto.response.TokenResponse
 import com.example.iceeteaserver.domain.auth.repository.RefreshTokenRepository
 import com.example.iceeteaserver.domain.auth.service.TokenReissuance
-import com.example.iceeteaserver.domain.auth.util.JwtTokenUtil
+import com.example.iceeteaserver.domain.auth.util.MemberUtil
+import com.example.iceeteaserver.domain.member.repository.MemberRepository
 import com.example.iceeteaserver.global.security.exception.ExpiredTokenException
 import com.example.iceeteaserver.global.security.exception.InvalidTokenException
-import com.example.iceeteaserver.global.security.jwt.JwtTokenProvider
+import com.example.iceeteaserver.global.security.jwt.JwtGenerator
+import com.example.iceeteaserver.global.security.jwt.JwtParser
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import javax.security.auth.login.AccountNotFoundException
 
 
 @Service
 class TokenReissueService(
     private val refreshTokenRepository: RefreshTokenRepository,
-    private val jwtTokenUtil: JwtTokenUtil,
-    private val jwtTokenProvider: JwtTokenProvider
+    private val memberRepository: MemberRepository,
+    private val jwtGenerator: JwtGenerator,
+    private val jwtParser: JwtParser
 ) : TokenReissuance{
 
     @Transactional(rollbackFor = [Exception::class])
     override fun execute(refreshToken: String): TokenResponse {
-        val email = jwtTokenProvider.exactEmailFromRefreshToken(refreshToken)
-        refreshTokenRepository.findByIdOrNull(email)
-            .let { it ?: throw ExpiredTokenException() }
-            .let {
-                if (it.refreshToken!=refreshToken) {
-                    throw InvalidTokenException()
-                }
-            }
-        return jwtTokenUtil.generateToken(email)
+        val parsedRefreshToken = jwtParser.parseRefreshToken(refreshToken) ?: throw InvalidTokenException()
+        val refreshToken = refreshTokenRepository.findByIdOrNull(parsedRefreshToken) ?: throw ExpiredTokenException()
+        val member = memberRepository.findByEmail(refreshToken.email) ?: throw AccountNotFoundException()
+
+        return jwtGenerator.generateToken(member.email,member.role)
     }
 }
