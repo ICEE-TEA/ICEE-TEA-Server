1bdf91b27ef3f42caca8e82cb71a85bdb64248e9
KimGyeongsuuu <sk62628504@gmail.com>
2023-05-20 11:32:41 +0900

    :recycle: 로그인 리펙토링

---
+++ src/main/kotlin/com/example/iceeteaserver/domain/auth/service/UserSignInService.kt
@@ -0,0 +1,8 @@
+package com.example.iceeteaserver.domain.auth.service
+
+import com.example.iceeteaserver.domain.auth.presentation.dto.MemberDto
+import com.example.iceeteaserver.domain.auth.presentation.dto.response.TokenResponse
+
+interface UserSignInService {
+    fun execute(memberDto: MemberDto) : TokenResponse
+}
---
+++ src/main/kotlin/com/example/iceeteaserver/domain/auth/service/impl/UserSignInServiceImpl.kt
@@ -0,0 +1,38 @@
+package com.example.iceeteaserver.domain.auth.service.impl
+
+import com.example.iceeteaserver.domain.auth.exception.MismatchPasswordException
+import com.example.iceeteaserver.domain.auth.presentation.dto.MemberDto
+import com.example.iceeteaserver.domain.auth.presentation.dto.response.TokenResponse
+import com.example.iceeteaserver.domain.auth.service.UserSignInService
+import com.example.iceeteaserver.domain.member.exception.MemberNotFoundException
+import com.example.iceeteaserver.domain.member.repository.MemberRepository
+import com.example.iceeteaserver.global.security.jwt.JwtGenerator
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+
+@Service
+class UserSignInServiceImpl(
+    private val memberRepository: MemberRepository,
+    private val passwordEncoder: PasswordEncoder,
+    private val jwtGenerator: JwtGenerator
+) : UserSignInService{
+
+    @Transactional(rollbackFor = [Exception::class])
+    override fun execute(memberDto: MemberDto): TokenResponse =
+        validateLogin(memberDto)
+            .let { jwtGenerator.generateToken(memberDto.email,memberDto.role) }
+
+    private fun validateLogin(memberDto: MemberDto) {
+        memberRepository.findByEmail(memberDto.email)
+            .let { it ?: throw MemberNotFoundException() }
+            .let { passwordEncoder.matches(memberDto.password, it.password) }
+            .let {
+                if (it)
+                    return
+                else throw MismatchPasswordException()
+            }
+    }
+
+}
