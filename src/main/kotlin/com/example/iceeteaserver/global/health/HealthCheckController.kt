2159a19ee67269b96d68451b7be3f65120157ce6
KimGyeongsuuu <sk62628504@gmail.com>
2023-05-20 12:39:37 +0900

    :sparkles: healthCheck 구현

--- src/main/kotlin/com/example/iceeteaserver/global/health/HealthCheckController.kt
+++ src/main/kotlin/com/example/iceeteaserver/global/health/HealthCheckController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HealthCheckController {
 
-    @GetMapping
+    @GetMapping("/health")
     fun healthCheck(): ResponseEntity<Map<String, String>> =
         ResponseEntity.ok(mapOf("message" to "IceeTea server running"))
 
