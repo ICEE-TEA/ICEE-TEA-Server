64b0fa97d276e1817286e64c250fdcbf8bc57480
KimGyeongsuuu <sk62628504@gmail.com>
2023-06-11 22:33:47 +0900

    :bug: mariadb 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2")
 
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
