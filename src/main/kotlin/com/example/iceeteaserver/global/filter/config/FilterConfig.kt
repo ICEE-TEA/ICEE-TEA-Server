7ffbb9572095509f4b542106ad4f195fb80fbb6d
KimGyeongsuuu <sk62628504@gmail.com>
2023-07-11 09:58:23 +0900

    :sparkles: :: FilterConfig 추가

---
+++ src/main/kotlin/com/example/iceeteaserver/global/filter/config/FilterConfig.kt
@@ -0,0 +1,20 @@
+package com.example.iceeteaserver.global.filter.config
+
+import com.example.iceeteaserver.global.filter.ExceptionHandlerFilter
+import com.example.iceeteaserver.global.filter.JwtRequestFilter
+import com.example.iceeteaserver.global.security.jwt.JwtParser
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+class FilterConfig(
+    private val jwtParser: JwtParser
+): SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+
+    override fun configure(builder: HttpSecurity) {
+        builder.addFilterBefore(JwtRequestFilter(jwtParser), UsernamePasswordAuthenticationFilter::class.java)
+        builder.addFilterBefore(ExceptionHandlerFilter(), JwtRequestFilter::class.java)
+    }
+
+}
